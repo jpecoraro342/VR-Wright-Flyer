7acf42e46586d7036f8e9faf99eb3e63a4fae03f
cfkwok <cfkwok@ufl.edu>
2016-02-20 09:43:54 -0500

    Fix wing warp jitter

--- WrightFlyerTour/Assets/Scripts/BackRudderController.cs
+++ WrightFlyerTour/Assets/Scripts/BackRudderController.cs
@@ -10,8 +10,12 @@ public class BackRudderController : MonoBehaviour {
 	private float initialObjPositionY;
 	private float objOffsetBackRudder = 20F;
 
-	// 0 degrees == 360 degrees... take that into account
-	private float overflowDegree = 0F;
+    // Keep track of whether or not it has been overotated. Prevents jitter
+    private bool leftRot = false;
+    private bool rightRot = false;
+
+    // 0 degrees == 360 degrees... take that into account
+    private float overflowDegree = 0F;
 
 	// Use this for initialization
 	void Start () {
@@ -31,30 +35,42 @@ public class BackRudderController : MonoBehaviour {
 	}
 
 	void backRudderControl() {
-		if (head.transform.eulerAngles.y > initialOffset.y + 2) {
-			float speed = (float) ((head.transform.eulerAngles.y - (initialOffset.y + 2)) / 20); 
-			if (transform.eulerAngles.y < initialObjPositionY + objOffsetBackRudder || transform.eulerAngles.y >= overflowDegree) {
-				transform.Rotate (0, speed, 0);
-
-			}
+		if (head.transform.eulerAngles.y > initialOffset.y + 2 && !leftRot) {
+			float speed = (float) ((head.transform.eulerAngles.y - (initialOffset.y + 2)) / 20);
+            //if (speed) 
+			
 
 			// Object may get stuck after moving it, so check that it does not over rotate
 			if ((transform.eulerAngles.y > initialObjPositionY + objOffsetBackRudder) && transform.eulerAngles.y < overflowDegree) {
 				Quaternion target = Quaternion.Euler (0, initialObjPositionY + objOffsetBackRudder - 0.1F, 0);
 				transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * smooth);
+                leftRot = true;
+                print("Left stopped");
+                print(transform.eulerAngles.y);
 			}
-		}
-		else if (head.transform.eulerAngles.y < initialOffset.y - 2) {
+            else if (transform.eulerAngles.y < initialObjPositionY + objOffsetBackRudder || transform.eulerAngles.y >= overflowDegree)
+            {
+                transform.Rotate(0, speed, 0);
+                rightRot = false;
+            }
+        }
+		else if (head.transform.eulerAngles.y < initialOffset.y - 2 && !rightRot) {
 			float speed = (float) ((head.transform.eulerAngles.y - (initialOffset.y - 2)) / 20); 
-			if (transform.eulerAngles.y < initialObjPositionY + objOffsetBackRudder || transform.eulerAngles.y >= overflowDegree) {
-				transform.Rotate (0, speed, 0);
-			}
+			
 
 			if ((transform.eulerAngles.y > initialObjPositionY + objOffsetBackRudder) && transform.eulerAngles.y < overflowDegree) {
 				Quaternion target = Quaternion.Euler (0, overflowDegree + 0.1F, 0);
 				transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * smooth);
+                rightRot = true;
+                print("Right stopped");
+                print(transform.eulerAngles.y);
 			}
-		}
+            else if (transform.eulerAngles.y < initialObjPositionY + objOffsetBackRudder || transform.eulerAngles.y >= overflowDegree)
+            {
+                transform.Rotate(0, speed, 0);
+                leftRot = false;
+            }
+        }
 	}
 
 }
--- WrightFlyerTour/Assets/Scripts/WingWarpController.cs
+++ WrightFlyerTour/Assets/Scripts/WingWarpController.cs
@@ -9,6 +9,8 @@ public class WingWarpController : MonoBehaviour {
 	private Vector3 initialOffset;
 	private float initialObjPositionZ;
 	private float objOffsetWingWarp = 3F;
+    private bool leftRot = false;
+    private bool rightRot = false;
 
 	// 0 degrees == 360 degrees... take that into account
 	private float overflowDegree = 0F;
@@ -25,38 +27,54 @@ public class WingWarpController : MonoBehaviour {
 		wingWarpControl ();
 	}
 
-	void wingWarpControl() {
+    void wingWarpControl() {
 		// Z rotation is different, starts at 0 instead of 180 (like Y) so we restrict its range
 
 		float paddingDegree = 2F;  // This is how much they can "tilt" before it starts to react
 		// Tilting CW
-		if (head.transform.eulerAngles.z > initialOffset.z + paddingDegree && head.transform.eulerAngles.z < 180) {
-			float speed = (float) ((head.transform.eulerAngles.z - (initialOffset.z + paddingDegree)) / 40); 
-			speed = Mathf.Min (0.17F, speed);
-			if (transform.eulerAngles.z < initialObjPositionZ + objOffsetWingWarp || transform.eulerAngles.z > 360 - objOffsetWingWarp) {
+		if (head.transform.eulerAngles.z > initialOffset.z + paddingDegree && head.transform.eulerAngles.z < 180 && !leftRot) {
+			float speed = (float) ((head.transform.eulerAngles.z - (initialOffset.z + paddingDegree)) / 40);
+			speed = Mathf.Min (0.1F, speed);
+            /*
+			if ((transform.eulerAngles.z < initialObjPositionZ + objOffsetWingWarp || transform.eulerAngles.z > 360 - objOffsetWingWarp)) {
 				transform.Rotate (0, 0, -speed);
 			}
+            */
 
 			// Object may get stuck after moving it, so check that it does not over rotate
 			if ((transform.eulerAngles.z < 360 - objOffsetWingWarp) && transform.eulerAngles.z > 180) {
 				
 				Quaternion target = Quaternion.Euler (0, 0, 360);
 				transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * smooth);
-			}
-		}
+                leftRot = true;
+            }
+            else if ((transform.eulerAngles.z < initialObjPositionZ + objOffsetWingWarp || transform.eulerAngles.z > 360 - objOffsetWingWarp))
+            {
+                transform.Rotate(0, 0, -speed);
+                rightRot = false;
+            }
+        }
 
 		// Tilting CCW
-		else if (head.transform.eulerAngles.z < 360 - paddingDegree && head.transform.eulerAngles.z > 180) {
+		else if (head.transform.eulerAngles.z < 360 - paddingDegree && head.transform.eulerAngles.z > 180 && !rightRot) {
 			float speed = (float) ((head.transform.eulerAngles.z - (360 - paddingDegree)) / 40); 
-			speed = Mathf.Max (-0.17F, speed);
-			if (transform.eulerAngles.z > 360 - objOffsetWingWarp || transform.eulerAngles.z < initialObjPositionZ + objOffsetWingWarp) {
+			speed = Mathf.Max (-0.1F, speed);
+            /*
+			if (transform.eulerAngles.z > 360 - objOffsetWingWarp || transform.eulerAngles.z < initialObjPositionZ + objOffsetWingWarp && transform.eulerAngles.z > initialObjPositionZ + objOffsetWingWarp)
+            {
 				transform.Rotate (0, 0, -speed);
 			}
-
+            */
 			if ((transform.eulerAngles.z > initialObjPositionZ + objOffsetWingWarp) && transform.eulerAngles.z < 180) {
 				Quaternion target = Quaternion.Euler (0, 0, 360 - objOffsetWingWarp + 0.01F);
 				transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * smooth);
+                rightRot = true;
 			}
-		}
+            else if (transform.eulerAngles.z > 360 - objOffsetWingWarp || transform.eulerAngles.z < initialObjPositionZ + objOffsetWingWarp)
+            {
+                transform.Rotate(0, 0, -speed);
+                leftRot = false;
+            }
+        }
 	}
 }
