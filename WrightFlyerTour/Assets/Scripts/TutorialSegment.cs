8a85d6fbc18692398a5f4b1e3021df2ede12a710
cfkwok <cfkwok@ufl.edu>
2016-02-23 22:49:52 -0500

    Add pitch animation and updated tutorial seg

--- WrightFlyerTour/Assets/Scripts/TutorialSegment.cs
+++ WrightFlyerTour/Assets/Scripts/TutorialSegment.cs
@@ -11,12 +11,13 @@ using System.Collections;
 */
 public class TutorialSegment : MonoBehaviour {
 
-	private bool completedWingL = false;
-	private bool completedWingR = false;
-	private bool completedBackRudL = false;
-	private bool completedBackRudR = false;
+	private bool completedWingL = true;
+	private bool completedWingR = true;
+	private bool completedBackRudL = true;
+	private bool completedBackRudR = true;
 	private bool completedFrontRudL = false;
 	private bool completedFrontRudR = false;
+	private bool gotOnPlane = false;
 
 	public Text tutText;
 	public Image panel;
@@ -38,12 +39,16 @@ public class TutorialSegment : MonoBehaviour {
 			backRudderTut();
 		}
 		/*
-		else if (!completedFrontRudR) {
-
+		else if (!gotOnPlane) {
+			getOnPlane();
 		}
 		*/
+		else if (!completedFrontRudR) {
+			frontRudderTut ();
+		}
 		else {
 			enabled = false;  // Turns off this script
+			tutText.text = "";
 			panel.gameObject.SetActive(false);
 		}
 	}
@@ -63,7 +68,7 @@ public class TutorialSegment : MonoBehaviour {
 			tutText.text = "Look right to move the rudder right";
 			print("Look right to move the rudder right.");
 			if (go.transform.eulerAngles.y > 18 && go.transform.eulerAngles.y < 180) {
-				tutText.text = "";
+				
 				print("Great job!");
 				completedBackRudR = true;
 				//enabled = false;
@@ -101,6 +106,33 @@ public class TutorialSegment : MonoBehaviour {
 	}
 
 	void frontRudderTut () {
+		GameObject go = GameObject.Find("Front Pivot");
+		if (!completedFrontRudL) {
+			tutText.text = "Now let's look down to pitch downwards";
+			if (go.transform.eulerAngles.x < 342 && go.transform.eulerAngles.x > 180) {
+				//tutText.CrossFadeAlpha(0.0f, 0.5f, false);
+				completedFrontRudL = true;
+			}
+		}
 
+		else {
+
+			tutText.text = "Let's try looking up";
+			//tutText.CrossFadeAlpha(1.0f, 2.5f, false);
+			if (go.transform.eulerAngles.x > 18 && go.transform.eulerAngles.x < 180) {
+				completedFrontRudR = true;
+				//enabled = false;
+			}
+
+
+		}
 	}
+
+/*
+	void getOnPlane() {
+		GameObject go = GameObject.Find("Character");
+		Vector3 first = new Vector3(-1, 0, 1);
+		go.transform.Translate(first * Time.deltaTime);
+	}
+*/
 }
