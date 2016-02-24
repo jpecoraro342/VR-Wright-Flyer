cb1fc2857859da54696d46c54670929a330611fe
jpecoraro342 <jpecoraro342@ufl.edu>
2016-02-23 22:05:33 -0500

    FLyer is basically completed

---
+++ WrightFlyerTour/Assets/_Scripts/ObjectReset.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectReset : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Cardboard.SDK.Triggered) {
+			resetRigidBody(gameObject);
+		}
+	}
+
+	IEnumerator resetRigidBody(GameObject gobject) {
+		Debug.Log("Reset Object");
+		gobject.transform.localPosition = Vector3.zero;
+
+		var rigidbody = gobject.GetComponent<Rigidbody>();
+
+		rigidbody.velocity = Vector3.zero;
+		rigidbody.angularVelocity = Vector3.zero;
+
+		rigidbody.Sleep();
+
+		yield return new WaitForSeconds(1.0f);
+
+		rigidbody.WakeUp();
+	}
+}
